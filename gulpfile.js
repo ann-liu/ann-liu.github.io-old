2891193bbf64e432f191f6c6ab69d1d891d73274
ann liu <annliu03@gmail.com>
2017-12-31 18:57:17 -0800

    Project config changes (build & dist folders)

--- gulpfile.js
+++ gulpfile.js
@@ -10,33 +10,33 @@ var gulp = require('gulp'),
 var minify = composer(uglify_es, console);
 
 gulp.task('sass', function() {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./build/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('styles.css'))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('css-min', ['sass'], function() {
-  return gulp.src('css/*.css')
-    .pipe(cleancss({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./css'));
+  return gulp.src('./dist/css/*.css')
+    .pipe(cleancss())
+    .pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('concat-js', function() {
-  return gulp.src('./js/*.js')
+  return gulp.src('./build/js/*.js')
     .pipe(concat('main.js'))
-    .pipe(gulp.dest('./js-min'));
+    .pipe(gulp.dest('./dist/js'));
 });
 
 gulp.task('js-min', ['concat-js'], function() {
-  return gulp.src('./js-min/*.js')
+  return gulp.src('./dist/js/*.js')
     .pipe(minify({ mangle: true}))
-    .pipe(gulp.dest('./js-min'))
+    .pipe(gulp.dest('./dist/js'))
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./sass/**/*.scss', ['css-min']);
-  gulp.watch('./js/*.js', ['js-min']);
+  gulp.watch('./build/sass/**/*.scss', ['css-min']);
+  gulp.watch('./build/js/*.js', ['js-min']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['css-min', 'js-min', 'watch']);
